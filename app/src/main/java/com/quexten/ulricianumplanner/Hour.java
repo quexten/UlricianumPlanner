37f9538df1641191b5fd90e024af05a104bb12a9
Bernd Schoolmann <quextenmail@googlemail.com>
2016-12-13 20:43:12 +0100

    Fix #12
    As stated in #12, it was not possible to
    enter a subject in the 9 - 10th hour.
    This timeslot has been squeezed into the
    8 - 9th hr timeslot.

--- app/src/main/java/com/quexten/ulricianumplanner/Hour.java
+++ app/src/main/java/com/quexten/ulricianumplanner/Hour.java
@@ -10,7 +10,7 @@ public enum Hour {
     public static Hour fromString(String string) {
         Hour hour = (string.equals("3") || string.equals("3 - 4") || string.equals("4")) ? Hour.THREFOUR : Hour.ONETWO;
         hour = (string.equals("5") || string.equals("5 - 6") || string.equals("6")) ? Hour.FIVESIX : hour;
-        hour = (string.equals("8") || string.equals("8 - 9") || string.equals("9")) ? Hour.EIGHTNINE : hour;
+        hour = (string.equals("8") || string.equals("8 - 9") || string.equals("9") || string.equals("9 - 10")) ? Hour.EIGHTNINE : hour;
         hour = (string.equals("10") || string.equals("10 - 11") || string.equals("10 - 12")|| string.equals("11") || string.equals("12")) ? Hour.TENELEVEN : hour;
         return hour;
     }
