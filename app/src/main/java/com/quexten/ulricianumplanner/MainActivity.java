9b35a1896c8de74bd9501c877d7693e00a6ea3e7
Bernd Schoolmann <quextenmail@googlemail.com>
2016-12-23 16:45:57 +0100

    Remove unused import

--- app/src/main/java/com/quexten/ulricianumplanner/MainActivity.java
+++ app/src/main/java/com/quexten/ulricianumplanner/MainActivity.java
@@ -6,7 +6,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
