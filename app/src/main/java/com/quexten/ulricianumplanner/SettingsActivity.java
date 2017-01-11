eb5f554a34fa9065255fd40e9c2ea070f2358347
Bernd Schoolmann <quextenmail@googlemail.com>
2017-01-11 18:52:00 +0100

    Merge settings into one screen
    This commit merges the 2 settings
    categories into one screen since there
    weren't that many settings.

--- app/src/main/java/com/quexten/ulricianumplanner/SettingsActivity.java
+++ app/src/main/java/com/quexten/ulricianumplanner/SettingsActivity.java
@@ -3,22 +3,16 @@ package com.quexten.ulricianumplanner;
 
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.os.Build;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.support.v7.app.ActionBar;
 import android.preference.PreferenceFragment;
-import android.view.MenuItem;
+import android.util.Log;
 import android.widget.ArrayAdapter;
-
-import com.google.firebase.messaging.FirebaseMessaging;
-
-import java.util.List;
 
 /**
  * A {@link PreferenceActivity} that presents a set of application settings. On
@@ -33,47 +27,92 @@ import java.util.List;
  */
 public class SettingsActivity extends AppCompatPreferenceActivity {
 
-    /**
-     * Helper method to determine if the device has an extra-large screen. For
-     * example, 10" tablets are extra-large.
-     */
-    private static boolean isXLargeTablet(Context context) {
-        return (context.getResources().getConfiguration().screenLayout
-                & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setupActionBar();
+        addPreferencesFromResource(R.xml.pref_general);
+
+        final CoursePlan coursePlan = new CoursePlan(this.getApplicationContext(), null);
+
+        final Preference classPreference = findPreference("className");
+        coursePlan.readClassName();
+
+        classPreference.setSummary(coursePlan.className);
+        classPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                AlertDialog.Builder builderSingle = new AlertDialog.Builder(SettingsActivity.this);
+                builderSingle.setTitle("Klasse Auswählen");
+
+                final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                        SettingsActivity.this.getApplicationContext(),
+                        R.layout.class_chooser);
+                arrayAdapter.add("5");
+                arrayAdapter.add("6");
+                arrayAdapter.add("7");
+                arrayAdapter.add("8");
+                arrayAdapter.add("9");
+                arrayAdapter.add("10");
+                arrayAdapter.add("11");
+                arrayAdapter.add("12");
+
+                builderSingle.setAdapter(
+                        arrayAdapter,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                coursePlan.className = arrayAdapter.getItem(which);
+                                coursePlan.saveClassName();
+                                classPreference.setSummary(coursePlan.className);
+                            }
+                        });
+
+                builderSingle.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which)
+                    {
+                        coursePlan.className = arrayAdapter.getItem(which);
+                        coursePlan.saveClassName();
+                        classPreference.setSummary(coursePlan.className);
+                    }
+                });
+                builderSingle.setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builderSingle.show();
+                return true;
+            }
+        });
+
+        findPreference("timetable_refresh").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                new TimetableFetchTask(SettingsActivity.this, coursePlan, null).execute();
+                return true;
+            }
+        });
+        findPreference("developer")
+                .setOnPreferenceClickListener(
+                        new Preference.OnPreferenceClickListener() {
+                            @Override
+                            public boolean onPreferenceClick(Preference preference) {
+                                Intent intent = new Intent(Intent.ACTION_VIEW);
+                                intent.setData(Uri.parse("http://quexten.com/about/"));
+                                startActivity(intent);
+                                return true;
+                            }
+                        });
     }
 
-    /**
-     * Set up the {@link android.app.ActionBar}, if the API is available.
-     */
     private void setupActionBar() {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
-            // Show the Up button in the action bar.
             actionBar.setDisplayHomeAsUpEnabled(false);
         }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean onIsMultiPane() {
-        return isXLargeTablet(this);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public void onBuildHeaders(List<Header> target) {
-        loadHeadersFromResource(R.xml.pref_headers, target);
     }
 
     /**
@@ -81,125 +120,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
      * Make sure to deny any unknown fragments here.
      */
     protected boolean isValidFragment(String fragmentName) {
-        return PreferenceFragment.class.getName().equals(fragmentName)
-                || GeneralPreferenceFragment.class.getName().equals(fragmentName)
-                || NotificationPreferenceFragment.class.getName().equals(fragmentName);
-    }
-
-    /**
-     * This fragment shows general preferences only. It is used when the
-     * activity is showing a two-pane settings UI.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class GeneralPreferenceFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_general);
-            setHasOptionsMenu(true);
-
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
-
-            final CoursePlan coursePlan = new CoursePlan(this.getActivity().getApplicationContext(), null);
-
-            final Preference classPreference = findPreference("className");
-            coursePlan.readClassName();
-
-            classPreference.setSummary(coursePlan.className);
-            classPreference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    AlertDialog.Builder builderSingle = new AlertDialog.Builder(GeneralPreferenceFragment.this.getActivity());
-                    builderSingle.setTitle("Klasse Auswählen");
-
-                    final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
-                            GeneralPreferenceFragment.this.getActivity(),
-                            android.R.layout.select_dialog_singlechoice);
-                    arrayAdapter.add("5");
-                    arrayAdapter.add("6");
-                    arrayAdapter.add("7");
-                    arrayAdapter.add("8");
-                    arrayAdapter.add("9");
-                    arrayAdapter.add("10");
-                    arrayAdapter.add("11");
-                    arrayAdapter.add("12");
-
-                    builderSingle.setAdapter(
-                            arrayAdapter,
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    coursePlan.className = arrayAdapter.getItem(which);
-                                    coursePlan.saveClassName();
-                                    classPreference.setSummary(coursePlan.className);
-                                }
-                            });
-
-                    builderSingle.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which)
-                        {
-                            coursePlan.className = arrayAdapter.getItem(which);
-                            coursePlan.saveClassName();
-                            classPreference.setSummary(coursePlan.className);
-                        }
-                    });
-                    builderSingle.setNegativeButton("Abbrechen", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int which) {
-                            dialog.dismiss();
-                        }
-                    });
-                    builderSingle.show();
-                    return true;
-                }
-            });
-
-            findPreference("className").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-                @Override
-                public boolean onPreferenceClick(Preference preference) {
-                    new TimetableFetchTask(getActivity(), coursePlan, null).execute();
-                    return true;
-                }
-            });
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            int id = item.getItemId();
-            if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
-    }
-
-    /**
-     * This fragment shows notification preferences only. It is used when the
-     * activity is showing a two-pane settings UI.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class NotificationPreferenceFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_notification);
-            setHasOptionsMenu(true);
-
-        }
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            int id = item.getItemId();
-            if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
+        return PreferenceFragment.class.getName().equals(fragmentName);
     }
 
 }
