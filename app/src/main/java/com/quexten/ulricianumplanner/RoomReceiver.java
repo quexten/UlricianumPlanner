d637bb665d4c56b0bbe3a7b7f77101ffdad481a7
Bernd Schoolmann <quextenmail@googlemail.com>
2017-02-06 19:18:02 +0100

    Fix #23

--- app/src/main/java/com/quexten/ulricianumplanner/RoomReceiver.java
+++ app/src/main/java/com/quexten/ulricianumplanner/RoomReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import java.util.Calendar;
+
 /**
  * Created by Quexten on 15-Sep-16.
  */
@@ -12,6 +14,12 @@ public class RoomReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        //Prevent notifications from appearing on the weekend
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        if(day == Calendar.SATURDAY || day == Calendar.SUNDAY)
+            return;
+
         new NextRoomTask(context).execute();
     }
 
