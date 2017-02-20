c3d746b281feace658972c1c3bb2dfc128aaff3f
Bernd Schoolmann <quextenmail@googlemail.com>
2017-02-20 17:46:43 +0100

    Move hardcoded text to strings.xml

--- app/src/main/java/com/quexten/ulricianumplanner/NotificationPoster.java
+++ app/src/main/java/com/quexten/ulricianumplanner/NotificationPoster.java
@@ -84,7 +84,7 @@ public class NotificationPoster {
                         .setContentText(message)
                         .setColor(Color.argb(255, 196, 0, 0))
                         .setLights(Color.argb(255, 255, 255, 0), 500, 500)
-                        .addAction(R.drawable.ic_share, "Teilen", getSharingIntent(context, message));
+                        .addAction(R.drawable.ic_share, context.getResources().getString(R.string.share_button), getSharingIntent(context, message));
 
         Intent resultIntent = new Intent(context, MainActivity.class);
 
