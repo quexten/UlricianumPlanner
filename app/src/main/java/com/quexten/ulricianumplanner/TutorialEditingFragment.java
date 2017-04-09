b64aa66b3965d6d929d5ba17b82f2362defc8933
Bernd Schoolmann <quextenmail@googlemail.com>
2017-04-09 18:36:37 +0200

    Remove unused imports

--- app/src/main/java/com/quexten/ulricianumplanner/TutorialEditingFragment.java
+++ app/src/main/java/com/quexten/ulricianumplanner/TutorialEditingFragment.java
@@ -1,12 +1,10 @@
 package com.quexten.ulricianumplanner;
 
-import android.app.DialogFragment;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * Created by Quexten on 27-Jan-17.
