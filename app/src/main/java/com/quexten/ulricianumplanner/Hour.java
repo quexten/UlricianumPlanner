6a1fd99bdda3032cb69afd2780ff71c7e952bf9a
Bernd Schoolmann <quextenmail@googlemail.com>
2017-04-26 06:26:20 +0200

    Fix string comparison order

--- app/src/main/java/com/quexten/ulricianumplanner/Hour.java
+++ app/src/main/java/com/quexten/ulricianumplanner/Hour.java
@@ -8,10 +8,10 @@ public enum Hour {
     ONETWO, THREFOUR, FIVESIX, EIGHTNINE, TENELEVEN;
     
     public static Hour fromString(String string) {
-        Hour hour = (string.equals("3") || string.equals("3 - 4") || string.equals("4")) ? Hour.THREFOUR : Hour.ONETWO;
-        hour = (string.equals("5") || string.equals("5 - 6") || string.equals("6")) ? Hour.FIVESIX : hour;
-        hour = (string.equals("8") || string.equals("8 - 9") || string.equals("9") || string.equals("9 - 10")) ? Hour.EIGHTNINE : hour;
-        hour = (string.equals("10") || string.equals("10 - 11") || string.equals("10 - 12")|| string.equals("11") || string.equals("12")) ? Hour.TENELEVEN : hour;
+        Hour hour = ("3".equals(string) || "3 - 4".equals(string) || "4".equals(string)) ? Hour.THREFOUR : Hour.ONETWO;
+        hour = ("5".equals(string) || "5 - 6".equals(string) || "6".equals(string)) ? Hour.FIVESIX : hour;
+        hour = ("8".equals(string) || "8 - 9".equals(string) || "9".equals(string) || "9 - 10".equals(string)) ? Hour.EIGHTNINE : hour;
+        hour = ("10".equals(string) || "10 - 11".equals(string) || "10 - 12".equals(string)|| "11".equals(string) || "12".equals(string)) ? Hour.TENELEVEN : hour;
         return hour;
     }
 
