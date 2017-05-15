d4c60d8b9cc9e9dc1e2c5b90eb836637d18814e7
Bernd Schoolmann <quextenmail@googlemail.com>
2017-05-15 14:32:32 +0200

    Add basic syncadapter implementation

---
+++ app/src/main/java/com/quexten/ulricianumplanner/sync/SubstitutionProvider.java
@@ -0,0 +1,18 @@
+package com.quexten.ulricianumplanner.sync;
+
+import com.quexten.ulricianumplanner.substitutions.Substitutions;
+
+/**
+ * Created by Quexten on 15-May-17.
+ * A substitution provider must
+ */
+
+public interface SubstitutionProvider {
+
+    public Substitutions getSubstitutions();
+
+    //News
+
+    //Synchronize
+
+}
